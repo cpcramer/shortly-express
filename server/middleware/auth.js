21c2e4851f330aaddecfc17f0c0c527169ee328f
Chadcramer <chadpcramer@gmail.com>
2019-05-04 12:22:15 -0500

    passing 21 tests

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -3,18 +3,54 @@ const Promise = require('bluebird');
 const _ = require('lodash')
 
 module.exports.createSession = (req, res, next) => {
+  req.session = {};
   if (_.isEmpty(req.cookies)) {
     models.Sessions.create()
       .then((session) => {
-        //console.log(models.Sessions.get({ hash }))
-        req.session = session
-        console.log(req.session)
+        return models.Sessions.get({ "id": session.insertId })
+      })
+      .then((results) => {
+        req.session = results
+        req.session.hash = results.hash;
+        res.cookies['shortlyid'] = { 'value': results.hash }
+        next();
       })
       .catch(() => {
         console.log('failed')
+        res.send();
+        next();
       })
+  } else {
+    models.Sessions.get({ "hash": req.cookies.shortlyid })
+      .then((results) => {
+        if (!results) {
+          models.Sessions.create()
+            .then((session) => {
+              return models.Sessions.get({ "id": session.insertId })
+            })
+            .then((results) => {
+              req.session = results
+              req.session.hash = results.hash;
+              res.cookies['shortlyid'] = { 'value': results.hash }
+              next();
+            })
+            .catch(() => {
+              console.log('failed')
+              res.send();
+              next();
+            })
+        } else {
+          req.session = results
+          next();
+        }
+      })
+      .catch(() => {
+        console.log('failed')
+        res.send();
+        next();
+      })
+    //next();
   }
-  next();
 };
 
 /************************************************************/
